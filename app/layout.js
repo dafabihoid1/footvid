6ba663adf7117b8df7ea2fd9169243af18eda21f
Fabian Weiß <fabianweiss28@gmail.com>
2025-05-28 15:05:21 +0200

    pwa 7

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="de" className="dark">
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+<link rel="apple-touch-icon" href="/icons/icon-192.png" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
