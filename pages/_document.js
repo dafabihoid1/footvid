c4177e21a73b67e1b1f623ffa7c8eecfc5606a1c
Fabian Weiß <fabianweiss28@gmail.com>
2025-05-28 14:07:42 +0200

    pwa 2

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/public/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         {/* <link rel="apple-touch-icon" href="/icons/icon-192.png" /> */}
